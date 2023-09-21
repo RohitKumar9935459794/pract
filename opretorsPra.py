016a53bb872fcb140328ddeb718d9d9caf2296ed
DELL <rohit307507@gmail.com>
2023-09-21 13:33:03 +0530

    ope

---
+++ opretorsPra.py
@@ -0,0 +1,72 @@
+# Mathematical operator
+print(12+13)
+print(12-13)
+print(12*13)
+print(12/13)
+print(12//13)
+print(12%13)
+print(12**13)
+
+# Comparison operator
+print(12>13)
+print(13<12)
+print(12==13)
+print(12!=13)
+print(11>=11)
+print(11<=11)
+
+# logical operator
+x= True
+y= False
+
+print(x and y)
+print( x or y)
+print(not x)
+print( not y)
+
+
+#bitwise operator
+x1 =10
+y1 = 4
+print( x1|y1)# bitwise or
+print(x1&y1)# bitewise  and
+print(~x1)# bitwise not
+print(x1^y1) #Bitwise xor
+print(x1>>2) #bitwise right shift
+print( y1>>2 ) # bitewise left shift
+
+# Assignment operator
+# p=2
+# p+=4
+# p -=3
+# *= , /= ,%= , //= , **= ,&= ,!=, ^=, >>=,<<=
+
+
+
+# identity operator
+
+
+ b7 = 5
+ p2 =5
+ q1= 'hello'
+ p3 = 'hello'
+ p4= [1,2,3]
+ p5 =[1,2,3]
+
+# print(b7 is not p2)
+ print(q1 is p3)
+ print(p4 is p5)
+
+
+
+ # Membership operator
+ x7= ' Hello world'
+ y7 = {1:'a,2:'b}
+
+ print(H in x7)
+
+
+
+
+
+
